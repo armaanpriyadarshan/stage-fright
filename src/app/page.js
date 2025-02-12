4200fc11aae8afc0f2975828d49c8ea36a21e598
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2025-02-11 21:36:49 -0500

    asset changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <Image
-        src="/images/hero.jpg"
+        src="/images/hero.png"
         alt="Stage Fright"
-        width={500}
-        height={500}
+        width={600}
+        height={600}
       />
     </main>
   );
