5c9d56f266ba7ca267130e6c383ce1b8e97477e7
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2025-02-12 21:06:15 -0500

    utterly cooked

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
+"use client";
+
+import { useRef } from "react";
 import Member from "@/components/member";
 import Navigation from "@/components/navigation";
 import Image from "next/image";
 
 export default function Home() {
+  const aboutRef = useRef(null);
+
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Navigation />
 
       {/* hero container */}
-      <div className="flex flex-col items-center min-h-screen mt-24`">
+      <div className="flex flex-col items-center min-h-[80vh] pt-12">
         <Image
           src="/images/hero.png"
           alt="Stage Fright"
@@ -18,7 +27,7 @@ export default function Home() {
         <h1 className="mt-12 text-2xl font-roboto text-white uppercase tracking-[0.5em]">
           frightening a stage near you
         </h1>
-        <div className="mt-8">
+        <div className="mt-8 cursor-pointer" onClick={scrollToAbout}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -32,19 +41,34 @@ export default function Home() {
         </div>
       </div>
 
-      {/* about container */}
-      <div className="flex flex-col items-center min-h-screen">
+      {/* About container */}
+      <div ref={aboutRef} className="flex flex-col items-center min-h-[80vh] w-3/5 pt-24">
         <p className="text-2xl font-roboto text-white tracking-[0.4em]">*****************</p>
         <h2 className="text-6xl font-prata text-white tracking-[0.4em] py-6">
           about
         </h2>
         <p className="text-2xl font-roboto text-white tracking-[0.4em]">***************</p>
         <div className="flex items-center gap-x-12 mt-12">
-          <Member
-            src="/images/armaan.jpg"
-            name="armaan 'chuti' priyadarshan"
-            description="Lead Singer"
-          />
+          <Member src="/images/armaan.jpg" name="armaan" description="guitar" />
+          <Member src="/images/ethan.jpg" name="ethan" description="piano" />
+          <Member src="/images/rishi.jpg" name="rishi" description="vocals" />
+          <Member src="/images/shaurya.jpg" name="shaurya" description="percussion" />
+        </div>
+        <div className = "flex flex-col md:flex-row items-center justify-center w-full mt-12">
+          <div className="w-full md:w-1/2 p-6">
+            <h1 className="text-3xl text-white font-prata mb-4">who we are</h1>
+            <p className="text-lg text-justify text-white font-roboto">
+              About us...
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 p-6 flex justify-center">
+            <Image
+              src="/images/stage.png"
+              alt="Stage Fright"
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
       </div>
     </main>
--- src/components/member.js
+++ src/components/member.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Member({ src, name, description }) {
     return (
         <div className="flex flex-col items-center p-4 max-w-[350px]">
-            <Image src={src} alt="Member Image" width={300} height={300} className="rounded-full" />
-            <h2 className="mt-8 text-2xl font-prata italic uppercase tracking-[0.2em] text-center">{name}</h2>
-            <p className="mt-2 text-gray-600 text-center">{description}</p>
+            <Image src={src} alt="Member Image" width={250} height={250} className="rounded-full" />
+            <h2 className="mt-8 text-2xl font-prata text-white italic uppercase tracking-[0.2em] text-center">{name}</h2>
+            <p className="mt-2 text-xl font-roboto text-gray-400 text-center">{description}</p>
         </div>
     );
 }
